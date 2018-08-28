d2570351284013bf1178b778a9271f5e637a8bf3
Kumar <gunajn3.kumar@citi.com>
2018-08-28 01:34:10 -0400

    removed dependency on level db due to version mismatch and deployment issues, now directly save data in json file

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var store = require('./datastore');
+//var store = require('./datastore');
+var fs = require('fs');
 var bodyParser = require('body-parser');
 const uuid = require('uuid/v1');
 const allConfig = 'allConfig';
@@ -14,11 +15,11 @@ const resultData = () => {
 
 
 var app = express(); // better instead
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 app.use(express.static(__dirname + '/dist/ng-drag-drop-app'));
 app.use(express.static(__dirname + '/public'));
@@ -34,24 +35,22 @@ app.set('port', process.env.PORT || 3000);
 // GET Enpoint for storing configs
 app.get('/config', function (req, res) {
 
-    store.fetch(allConfig, (err, value) => {
 
-        if (value) {
-            console.log('Data in DB ' + value);
-            //console.log('Data Retrieved : ' + JSON.stringify(value));
-
-            let result = resultData();
-            result.data = value;
-            result.message = "Success while Fetching record";
-            res.status(200).send(result);
-        } else {
-            console.log('Error  in DB ' + err)
-            let result = resultData();
+    fs.readFile('myjsonfile.json', function (err, content) {
+        let result = {};
+        if (err) {
             result.error = err;
             result.message = "Error while Fetching record";
             res.status(500).send(result);
+        } else{
+            var parseJson = JSON.parse(content);
+       
+            result.data = parseJson;
+            result.message = "Success while Fetching record";
+            res.status(200).send(result);
         }
-    });
+    })
+
 
 
 
@@ -60,26 +59,17 @@ app.get('/config', function (req, res) {
 // POST endpoint for saving configs
 app.post('/config', function (req, res) {
 
-    var configs = req.body
-    
-   // console.log('request payload :: ' + JSON.stringify(configs))
+    var json = req.body;
+    var jsonString = JSON.stringify(req.body);
 
-    store.save(allConfig, configs, (err, value) => {
-        if (value) {
-            console.log("Data Persist " + value)
-            let result = resultData();
-            result.data = value;
-            result.message = "Success while Saving record";
-            res.status(200).send(result);
-
-        } else {
-            console.log('Error  in DB ' + err)
-            let result = resultData();
-            result.error = err;
-            result.message = "Error while Saving record";
-            res.status(500).send(result);
-        }
+    // console.log('request payload :: ' + JSON.stringify(configs))
+    fs.writeFile('myjsonfile.json', jsonString, 'utf8', () => {
+        let result = {};
+        result.data = json;
+        result.message = "Success while Saving record";
+        res.status(200).send(result);
     });
+
 
 });
 
@@ -90,33 +80,33 @@ app.get('/view/:id', function (req, res) {
     console.log("id in the path variable " + id);
     if (id != null && typeof id != undefined) {
 
-        store.fetchHtml(id, (err, value) => {
-            if (value) {
-                let result = resultData();
-                result.id = id;
-                result.message = "Retrieved Html from database";
-                result.data = value;
-                console.log("Data retrieved from database returning 200 OK");
-                res.status(200).send(value)
-            }
-            else {
+        // store.fetchHtml(id, (err, value) => {
+        //     if (value) {
+        //         let result = resultData();
+        //         result.id = id;
+        //         result.message = "Retrieved Html from database";
+        //         result.data = value;
+        //         console.log("Data retrieved from database returning 200 OK");
+        //         res.status(200).send(value)
+        //     }
+        //     else {
 
-                let result = resultData();
-                result.message = "Error retreiving Html from database";
-                result.error=err;
-                console.log("Error in storing data in database returning 500");
-                res.status(500).send(result);
-            }
+        //         let result = resultData();
+        //         result.message = "Error retreiving Html from database";
+        //         result.error=err;
+        //         console.log("Error in storing data in database returning 500");
+        //         res.status(500).send(result);
+        //     }
 
-        });
-    }else {
+        // });
+    } else {
 
         let result = resultData();
         result.message = "Id empty returning error";
         console.log("Data stored in database returning 500 OK");
         res.status(500).send(result);
     }
-    
+
 
 
 });
@@ -127,23 +117,23 @@ app.post('/view', function (req, res) {
     var id = uuid();
     console.log("UUId for the Payload " + id);
     console.log('request payload :: ' + req.body.html);
-    let key = store.saveHtml(id, payLoad, (err, value) => {
-        if (value) {
-            console.log("Data Persist " + value)
-            let result = resultData();
-            result.id=value;
-            result.data = "success";
-            result.message = "Success while Saving record";
-            res.status(200).send(result);
+    // let key = store.saveHtml(id, payLoad, (err, value) => {
+    //     if (value) {
+    //         console.log("Data Persist " + value)
+    //         let result = resultData();
+    //         result.id=value;
+    //         result.data = "success";
+    //         result.message = "Success while Saving record";
+    //         res.status(200).send(result);
 
-        } else {
-            console.log('Error  in DB ' + err)
-            let result = resultData();
-            result.error = err;
-            result.message = "Error while Saving record";
-            res.status(500).send(result);
-        }
-    });
+    //     } else {
+    //         console.log('Error  in DB ' + err)
+    //         let result = resultData();
+    //         result.error = err;
+    //         result.message = "Error while Saving record";
+    //         res.status(500).send(result);
+    //     }
+    // });
 
 });
 app.listen(app.get('port'));
